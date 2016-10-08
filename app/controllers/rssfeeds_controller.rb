d1067db7f3c57f21624fa3c7f5e71af7df6396d3
dadoo <dbummer@hotmail.fr>
2016-10-08 09:43:00 +0000

    save before crash

--- app/controllers/rssfeeds_controller.rb
+++ app/controllers/rssfeeds_controller.rb
@@ -1,13 +1,44 @@
 class RssfeedsController < ApplicationController
+  require 'rss'
+  require 'open-uri'
 
   def index
+    @rssfeeds = Rssfeed.all
+  end
+
+  def show
+    @rssfeed = Rssfeed.find(params[:id])
   end
 
   def new
   end
 
   def create
-    render plain: params[:rssfeed].inspect
+  @rssfeed = Rssfeed.new(rssfeed_params)
+
+  if @rssfeed.save
+    # si le flux est ok, on cree de suite les items qui le composent
+    create_items
+    redirect_to @rssfeed
+  else
+    render 'new'
   end
+end
+
+private
+  def rssfeed_params
+    params.require(:rssfeed).permit(:title, :feedurl)
+  end
+
+  def create_items
+    url = @rssfeed.feedurl
+    open(url) do |rss|
+      feed = RSS::Parser.parse(rss)
+      feed.items.each do |elem|
+        # comment faire item = new Item(elem.title, elem.date, rssfeed.id, status = 0) ... ?
+        #@item = Item.new
+        #puts "Item: #{elem.title}"
+      end
+    end
 
 end
