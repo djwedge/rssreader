3944b710e85b12241720a5785058ba3a795742db
dadoo <dbummer@hotmail.fr>
2016-10-08 19:50:27 +0000

    unable to call item.update

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -6,24 +6,29 @@ class ItemsController < ApplicationController
   end
 
   def new
-    # this.title = title ...
-    @title = title
-    @summary = summary
-    @date = date
-    @url = url
-    @status = false # false pour non-lu, true pour lu
+    @status = false # false pour non-lu, true pour lu - n'a aucun effet ici
     # @item.save # ne fonctionne pas ici mais ok dans RssfeedsController (?)
   end
 
-  def update
+  def update # cette methode n'est pas appelee
     # methode pour mettre a jour le statut (non-lu / lu)
     @item = Item.find(params[:id])
-
-    if @item.update(status: !@item.status)
-      redirect_to @item
+    #puts 'HELLO JE SUIS LAAAAAAAAAAAAAAAAAA' #debug
+    #@item.update(title: "Pierre Tchernia est vivant") #debug
+    if @item.status
+      @item.update(status: 0)
+      puts non-lu #debug
     else
-      render 'show'
+      @item.update(status: 1)
+      puts lu #debug
     end
+    @item.save
+    redirect_to @rssfeeds
+    #if @item.update(status: !@item.status)
+    #  redirect_to @item
+    #else
+    #  render 'show'
+    #end
   end
 
 end
